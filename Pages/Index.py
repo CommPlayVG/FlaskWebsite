6fadcf2f8a9a58949a1bb23e6389c297046a87c8
CommPlayVG <Seanfricke03@gmail.com>
2022-02-10 11:19:12 -0500

    Make and Import Index.py externally

---
+++ Pages/Index.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+indexUrl = Blueprint("index", __name__)
+
+@indexUrl.route("/")
+def index():
+    return render_template("Pages/index.html")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from Pages.Index import indexUrl
 
 app = Flask(__name__)
 
+app.register_blueprint(indexUrl)
 
 if __name__ == "__main__":
     app.run(debug=True)
